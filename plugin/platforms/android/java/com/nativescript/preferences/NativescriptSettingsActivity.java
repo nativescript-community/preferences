e77ef67ddd52e6e62cd4a5de35b0a4f1babee6b8
Martin Guillon <martin.guillon@akylas.fr>
2020-02-24 10:31:33 +0100

    fix(android): android change events

--- plugin/platforms/android/java/com/nativescript/preferences/NativescriptSettingsActivity.java
+++ plugin/platforms/android/java/com/nativescript/preferences/NativescriptSettingsActivity.java
@@ -1,7 +1,14 @@
 package com.nativescript.preferences;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
+import androidx.preference.EditTextPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.MultiSelectListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceGroup;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,16 +24,65 @@ public class NativescriptSettingsActivity extends AppCompatActivity {
 
     }
 
-    public static class MainPreferenceFragment extends PreferenceFragmentCompat {
+    public static class MainPreferenceFragment extends PreferenceFragmentCompat implements OnSharedPreferenceChangeListener {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
         }
 
         @Override
+        public void onResume() {
+            super.onResume();
+            // Set up a listener whenever a key changes
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            // Unregister the listener whenever a key changes
+            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             getPreferenceManager().setSharedPreferencesName("prefs.db");
             setPreferencesFromResource(R.xml.preferences, rootKey);
+            initSummary(getPreferenceScreen());
+        }
+
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            updatePrefSummary(findPreference(key));
+        }
+
+        private void initSummary(Preference p) {
+            if (p instanceof PreferenceGroup) {
+                PreferenceGroup pGrp = (PreferenceGroup) p;
+                for (int i = 0; i < pGrp.getPreferenceCount(); i++) {
+                    initSummary(pGrp.getPreference(i));
+                }
+            } else {
+                updatePrefSummary(p);
+            }
+        }
+
+        private void updatePrefSummary(Preference p) {
+            if (p instanceof ListPreference) {
+                ListPreference listPref = (ListPreference) p;
+                p.setSummary(listPref.getEntry());
+            }
+            if (p instanceof EditTextPreference) {
+                EditTextPreference editTextPref = (EditTextPreference) p;
+                if (p.getTitle().toString().toLowerCase().contains("password")) {
+                    p.setSummary("******");
+                } else {
+                    p.setSummary(editTextPref.getText());
+                }
+            }
+            if (p instanceof MultiSelectListPreference) {
+                EditTextPreference editTextPref = (EditTextPreference) p;
+                p.setSummary(editTextPref.getText());
+            }
         }
     }
 }
