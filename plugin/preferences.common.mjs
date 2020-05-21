abfd53f4c2e1e44385274ffd6e3f33869aebdd71
Martin Guillon <martin.guillon@akylas.fr>
2020-05-21 15:16:06 +0200

    chore: cleanup

--- plugin/preferences.common.mjs
+++
@@ -1,7 +0,0 @@
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Common = void 0;
-const nativescript_observable_1 = require("nativescript-observable");
-class Common extends nativescript_observable_1.default {
-}
-exports.Common = Common;
-//# sourceMappingURL=preferences.common.mjs.map
