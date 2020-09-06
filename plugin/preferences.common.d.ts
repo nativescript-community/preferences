e5f162fca23907836836231a29f33cda45328ccd
Martin Guillon <martin.guillon@akylas.fr>
2020-09-06 23:15:57 +0200

    chore: cleanup

--- plugin/preferences.common.d.ts
+++
@@ -1,7 +0,0 @@
-import Observable from 'nativescript-observable';
-export declare abstract class Common extends Observable {
-    abstract setValue(key: string, value: any): any;
-    abstract getValue(key: string, defaultValue?: any): any;
-    abstract openSettings(): any;
-    abstract clear(): any;
-}
